a5169d65f63e3482c1573224365c7282fdef3759
plaristote <michael@unetresgrossebite.com>
2022-08-18 19:28:34 +0200

    update api for request parsers

--- libcrails-form-parser/form_parser.cpp
+++ libcrails-form-parser/form_parser.cpp
@@ -1,17 +1,18 @@
 #include "form_parser.hpp"
-#include <crails/params.hpp>
+#include <crails/context.hpp>
 #include <crails/url.hpp>
 
 using namespace std;
 using namespace Crails;
 
-void RequestFormParser::operator()(Connection& connection, BuildingResponse& out, Params& params, function<void(RequestParser::Status)> callback) const
+void RequestFormParser::operator()(Context& context, function<void(RequestParser::Status)> callback) const
 {
   static const regex is_form("application/x-www-form-urlencoded", regex_constants::extended);
+  const HttpRequest& request = context.connection->get_request();
 
-  if (params["method"].as<string>() != "GET" && content_type_matches(params, is_form))
+  if (request.method() != HttpVerb::get && content_type_matches(request, is_form))
   {
-    wait_for_body(connection, out, params, [callback]()
+    wait_for_body(context, [callback]()
     {
       callback(RequestParser::Stop);
     });
@@ -20,8 +21,8 @@ void RequestFormParser::operator()(Connection& connection, BuildingResponse& out
     callback(RequestParser::Continue);
 }
 
-void RequestFormParser::body_received(Connection&, BuildingResponse&, Params& params, const string& body) const
+void RequestFormParser::body_received(Context& context, const string& body) const
 {
   if (body.size() > 0)
-    cgi2params(params.as_data(), body);
+    cgi2params(context.params.as_data(), body);
 }
